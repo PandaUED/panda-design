dbd4e5ee325921f00cc3cdc2fcc32869a20b4f3c
canisminor <i@canisminor.cc>
2017-09-27 16:46:25 +0800

    fix md

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 // routes里的文件夹名要和组件名一一对应
 // 不同的组件要尽量拆成不同的文件夹，不要塞在一起
-// 文档请不要留别也会报错
-// 请至少复制Example中的文档
+// 文档请不要留白也会报错，至少复制Example中的文档
 export default {
   Variable: ['style'],
   Layout: ['View', 'WhiteSpace', 'WingBlank'],
