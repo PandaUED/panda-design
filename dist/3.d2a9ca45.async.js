4905db9e822bd1fe0232bd368a697eaf8255e6da
liqi <liqi@xmzhen.com>
2017-09-28 19:31:25 +0800

    remove dist

--- dist/3.d2a9ca45.async.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([3],{103:function(e,t,u){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var l=u(0),n=function(e){return e&&e.__esModule?e:{default:e}}(l),f=u(12);t.default=function(){return n.default.createElement(f.View,{color:"#fff"},n.default.createElement(f.Example,null))},e.exports=t.default}});
