a0da1ff53c8a425aa13bc2f749f799bda142207b
wenyunyan <kikooo_0421@163.com>
2017-09-30 11:43:54 +0800

    修改Avatar样例

--- src/routes/Avatar/index.js
+++ src/routes/Avatar/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { View, Avatar, WhiteSpace } from 'pand';
+import { View, List, Avatar, WhiteSpace } from 'pand';
 
 export default () => {
   return (
     <View>
-      <WhiteSpace />
-      <Avatar
-        shape="circle"
-        url="https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=3360175613,2775433479&fm=58&u_exp_0=2793141998,2442128069&fm_exp_0=86&bpow=870&bpoh=1280"
-      />
+      <WhiteSpace>头像</WhiteSpace>
+      <List>
+        <List.Item padding={16} split>
+          <Avatar url="https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=3360175613,2775433479&fm=58&u_exp_0=2793141998,2442128069&fm_exp_0=86&bpow=870&bpoh=1280" />
+        </List.Item>
+        <List.Item padding={16} split>
+          <Avatar
+            shape="square"
+            url="https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=3360175613,2775433479&fm=58&u_exp_0=2793141998,2442128069&fm_exp_0=86&bpow=870&bpoh=1280"
+          />
+        </List.Item>
+      </List>
       <WhiteSpace />
     </View>
   );
