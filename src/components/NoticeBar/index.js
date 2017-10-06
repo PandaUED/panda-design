40811c7722abddb3c7d50e1e1be2f23be3fefb12
wenyunyan <kikooo_0421@163.com>
2017-10-06 21:26:22 +0800

    添加NoticeBar及样例

--- src/components/NoticeBar/index.js
+++ src/components/NoticeBar/index.js
@@ -1,18 +1,25 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import { Component } from 'react';
 import { Icon } from 'pand';
 
 const props = {
-  closable: PropTypes.oneOf(['auto' | 'true' | 'false']),
+  closable: PropTypes.bool,
   marquee: PropTypes.bool,
   center: PropTypes.bool,
+  icon: PropTypes.node,
+  visible: PropTypes.bool,
+  onClose: PropTypes.func,
 };
 
 const NoticeBar = ({
-  closable = 'true',
+  closable,
   marquee,
   center,
+  icon,
   children = 'example',
+  visible = false,
+  onClose,
   ...other
 }) => {
   let NoticeBar = styled.div`
@@ -21,24 +28,26 @@ const NoticeBar = ({
     background: #fafbfb;
     height: 36px;
     line-height: 36px;
+    display: flex;
+    flex-direction: row;
   `;
-
+  let NoticeBarContent = styled.div``;
   if (center) {
-    NoticeBar = NoticeBar.extend`
-      display: flex;
-      flex-direction: row;
+    NoticeBarContent = NoticeBarContent.extend`
+      flex-grow: 1;
       align-items: center;
       justify-content: center;
+      display: flex;
     `;
   }
 
-  const NoticeBarIcon = styled.div`
-    right: 8px;
-    position: absolute;
-    display: inline-block;
+  const NoticeBarIcon = styled.a`
     span {
       line-height: 36px;
     }
+    padding: 0 8px;
+    display: inline-block;
+    background: #fafbfb;
   `;
 
   const fade = styled.div`
@@ -61,25 +70,29 @@ const NoticeBar = ({
       }
     }
   `;
-  if (marquee) {
+  if (visible) {
     return (
       <NoticeBar {...other}>
-        <marquee behavior="scroll" direction="left">
-          {children}
-        </marquee>
-      </NoticeBar>
-    );
-  } else {
-    return (
-      <NoticeBar {...other}>
-        {children}
-        {closable === 'true' && (
-          <NoticeBarIcon>
+        <NoticeBarIcon>{icon}</NoticeBarIcon>
+        {marquee && (
+          <marquee behavior="scroll" direction="left">
+            {children}
+          </marquee>
+        )}
+        {!marquee && <NoticeBarContent>{children}</NoticeBarContent>}
+        {closable && (
+          <NoticeBarIcon
+            onClick={() => {
+              onClose();
+            }}
+          >
             <Icon size={16} type="remove" color="#CACACA" />
           </NoticeBarIcon>
         )}
       </NoticeBar>
     );
+  } else {
+    return null;
   }
 };
 
