9c34b04763afd3dca6f37f960e8f3500feb526f7
wenyunyan <kikooo_0421@163.com>
2017-10-10 21:58:23 +0800

    Radio暂存

--- src/routes/Radio/index.js
+++ src/routes/Radio/index.js
@@ -6,12 +6,21 @@ export default () => {
     <View>
       <WhiteSpace>单选框</WhiteSpace>
       <List>
-        <List.Item padding={16} split>
-          <Radio />
-        </List.Item>
-        <List.Item padding={16} split>
-          <Radio />
-        </List.Item>
+        <Radio group="color" value="blue" name="蓝色" />
+        <Radio group="color" value="green" name="绿色" checked />
+        <Radio group="color" value="red" name="红色" />
+      </List>
+      <WhiteSpace />
+      <List>
+        <Radio group="color2" value="blue" name="蓝色" disabled checked />
+        <Radio group="color2" value="green" name="绿色" disabled />
+        <Radio group="color2" value="red" name="红色" disabled />
+      </List>
+      <WhiteSpace />
+      <List>
+        <Radio group="color3" value="blue" name="蓝色" desc="detail" />
+        <Radio group="color3" value="green" name="绿色" desc="detail" />
+        <Radio group="color3" value="red" name="红色" checked desc="detail" />
       </List>
       <WhiteSpace />
     </View>
