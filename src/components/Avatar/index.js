0300d9912a54d85e64bc1158937ea239af62daf2
wenyunyan <kikooo_0421@163.com>
2017-10-11 18:36:56 +0800

    Avatar修改

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,31 +1,59 @@
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { style } from '../';
 
 const props = {
   className: PropTypes.string,
   size: PropTypes.number,
   shape: PropTypes.string,
   url: PropTypes.string,
+  name: PropTypes.string,
 };
 
-const Image = ({ className, size, shape = 'circle', url, ...other }) => {
-  let Image = styled.img`background: transparent;`;
-  if (shape === 'circle') {
-    Image = Image.extend`
-      ${size
-        ? `width:${size}px;height:${size}px;border-radius:${size / 2}px`
-        : 'width:48px;height:48px;border-radius:24px'};
-    `;
-  } else if (shape === 'square') {
-    Image = Image.extend`
-      border-radius: 5px;
-      ${size ? `width:${size}px;height:${size}px;` : 'width:48px;height:48px;'};
-    `;
-  }
+const Avatar = ({
+  className,
+  size = 48,
+  shape = 'circle',
+  url,
+  name = ' ',
+  ...other
+}) => {
+  let circleStyle = css`border-radius: ${size / 2}px;`;
+  let squareStyle = css`border-radius: 5px;`;
 
-  return <Image className={className} {...other} src={url} width={size} height={size} />;
+  let AvatarWrap = styled.div`
+    display: flex;
+    align-items: center;
+  `;
+  let Avatar = styled.img`
+    background: ${style.color.bgDeactive};
+    width: ${size}px;
+    height: ${size}px;
+    ${shape === 'circle' && circleStyle};
+    ${shape === 'square' && squareStyle};
+  `;
+  let AvatarName = styled.div`
+    color: white;
+    text-align: center;
+    position: absolute;
+    width: ${size}px;
+    font-size: ${size * 0.6}px;
+  `;
+
+  return (
+    <AvatarWrap>
+      <Avatar
+        className={className}
+        {...other}
+        src={url}
+        width={size}
+        height={size}
+      />
+      {!url && <AvatarName>{name[0].toUpperCase()}</AvatarName>}
+    </AvatarWrap>
+  );
 };
 
-Image.propTypes = props;
+Avatar.propTypes = props;
 
-export default Image;
+export default Avatar;
