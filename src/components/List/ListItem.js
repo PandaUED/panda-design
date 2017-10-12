899985fcec4f70c17e6c0628c6516c8e60e691b5
wenyunyan <kikooo_0421@163.com>
2017-10-12 17:56:20 +0800

    List修改

--- src/components/List/ListItem.js
+++ src/components/List/ListItem.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import TouchFeedback from 'rmc-feedback';
 import PropTypes from 'prop-types';
 import { style } from '../';
@@ -20,31 +20,25 @@ const ListItem = ({
   padding = false,
   ...other
 }) => {
-  let ListItem = styled.div`
-    background: #fff;
+  const splitItem = css`
+    margin-bottom: 10px;
+    box-shadow: inset 0 0 0 0 ${style.color.bgPage};
+    ${style.split.bottom};
+  `;
+  const ListItem = styled.div`
+    background: ${style.color.white};
     width: 100%;
     font-size: 16px;
-    color: #444;
+    color: ${style.color.textDark};
     line-height: 16px;
     align-items: center;
-    box-shadow: inset 0 0 0 0 #f8f8f8;
+    box-shadow: inset 0 0 0 0 ${style.color.bgPage};
     ${padding ? `padding: 16px ${padding}px;` : 'padding: 0;'};
+    ${border && `border-bottom: ${style.color.bgPage} 1px solid;`};
+    ${padding && `padding: 16px ${padding}px;`};
+    ${padding && padding === true && 'padding: 16px 0;'};
+    ${split && splitItem};
   `;
-  if (padding && typeof padding === 'number') {
-    ListItem = ListItem.extend`${`padding: 16px ${padding}px;`};`;
-  } else if (padding) {
-    ListItem = ListItem.extend`padding: 16px 0;`;
-  }
-  if (split) {
-    ListItem = ListItem.extend`
-      margin-bottom: 10px;
-      box-shadow: inset 0 0 0 0 #f8f8f8;
-      ${style.split.bottom};
-    `;
-  }
-  if (border) {
-    ListItem = ListItem.extend`border-bottom: #f8f8f8 1px solid;`;
-  }
 
   return (
     <TouchFeedback>
