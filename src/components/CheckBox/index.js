334d6c31f2d77606e5a9e4292abb7311c4d44c88
kikooo <kikooo_0421@163.com>
2017-10-31 18:18:00 +0800

    checkbox新增协议展示类型

--- src/components/CheckBox/index.js
+++ src/components/CheckBox/index.js
@@ -1,110 +1,174 @@
+import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import { Icon } from 'pand';
 import { style } from '../style_index';
 
-const props = {
-  className: PropTypes.string,
-  title: PropTypes.string,
-  desc: PropTypes.string,
-  active: PropTypes.bool,
-  disabled: PropTypes.bool,
-  onChange: PropTypes.func,
-};
-
-const CheckBox = ({ title = 'example', className, active, disabled, onChange, desc, ...other }) => {
-  const CheckBoxWrap = styled.div`
-    display: flex;
-    align-items: center;
-  `;
-  const CheckBox = styled.div`
-    width: 21px;
-    height: 21px;
-    margin-right: 16px;
-    > div {
-      position: absolute;
-      width: 100%;
-      height: 44px;
-      input[type='checkbox']:checked + .checkboxicon {
-        border-color: ${style.color.blue};
-        background-color: ${style.color.blue};
-      }
-      input[type='checkbox']:disabled + .checkboxicon {
-        opacity: 0.3;
-      }
-    }
-  `;
-  const CheckBoxInput = styled.input`
+const CheckBoxWrap = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const CheckBoxView = styled.div`
+  width: 21px;
+  height: 21px;
+  margin-right: 16px;
+  > div {
     position: absolute;
-    top: 0;
-    left: 0;
-    opacity: 0;
     width: 100%;
-    height: 100%;
-    z-index: 2;
-    border: 0 none;
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    appearance: none;
-  `;
-  const CheckBoxIcon = styled.div`
-    position: relative;
-    right: 0;
-    width: 21px;
-    height: 21px;
-    border: 1px solid ${style.color.bgDeactive};
-    border-radius: 50%;
-    -webkit-transform: rotate(0deg);
-    -ms-transform: rotate(0deg);
-    transform: rotate(0deg);
-    box-sizing: border-box;
-    :after {
-      content: ' ';
-      width: 6px;
-      height: 10px;
-      position: absolute;
-      border-style: solid;
-      border-width: 0 1px 1px 0;
-      border-color: white;
-      transform: rotate(45deg);
-      left: 6px;
-      top: 2px;
+    height: 44px;
+    input[type='checkbox']:checked + .checkboxicon {
+      border-color: ${style.color.blue};
+      background-color: ${style.color.blue};
     }
-  `;
-  const CheckBoxContent = styled.div`
-    color: ${style.color.textDark};
-    ${disabled && `color: ${style.color.placeholder};`};
-  `;
-  const CheckBoxDesc = styled.div`
-    color: ${style.color.textLight};
-    font-size: 12px;
-  `;
+    input[type='checkbox']:disabled + .checkboxicon {
+      opacity: 0.3;
+    }
+  }
+`;
+const CheckBoxInput = styled.input`
+  position: absolute;
+  top: 0;
+  left: 0;
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 2;
+  border: 0 none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+`;
+const CheckBoxIcon = styled.div`
+  position: relative;
+  right: 0;
+  width: 21px;
+  height: 21px;
+  border: 1px solid ${style.color.bgDeactive};
+  border-radius: 50%;
+  -webkit-transform: rotate(0deg);
+  -ms-transform: rotate(0deg);
+  transform: rotate(0deg);
+  box-sizing: border-box;
+  :after {
+    content: ' ';
+    width: 6px;
+    height: 10px;
+    position: absolute;
+    border-style: solid;
+    border-width: 0 1px 1px 0;
+    border-color: white;
+    transform: rotate(45deg);
+    left: 6px;
+    top: 2px;
+  }
+`;
+const CheckBoxDesc = styled.div`
+  color: ${style.color.textLight};
+  font-size: 12px;
+`;
 
-  return (
-    <CheckBoxWrap className={className}>
-      <CheckBox>
-        <div>
-          <CheckBoxInput
-            type="checkbox"
-            {...other}
-            disabled={disabled}
-            defaultChecked={active}
-            onChange={e => {
-              if (onChange) {
-                onChange(e);
-              }
-            }}
-          />
-          <CheckBoxIcon className="checkboxicon" />
-        </div>
-      </CheckBox>
-      <CheckBoxContent>
-        {title}
-        {desc && <CheckBoxDesc>{desc}</CheckBoxDesc>}
-      </CheckBoxContent>
-    </CheckBoxWrap>
-  );
-};
+const SmallCheckBoxView = styled.div`
+  width: 16px;
+  height: 16px;
+  > div {
+    position: absolute;
+    width: 100%;
+    height: 20px;
+  }
+`;
 
-CheckBox.propTypes = props;
+const SmallCheckBoxContent = styled.div`
+  font-size: 12px;
+  line-height: 16px;
+  margin-left: 6px;
+  color: #999999;
+`;
+
+const SmallCheckBoxAgreement = styled.span`color: #444444;`;
+
+class CheckBox extends React.Component {
+  static propTypes = {
+    className: PropTypes.string,
+    title: PropTypes.string,
+    desc: PropTypes.string,
+    active: PropTypes.bool,
+    disabled: PropTypes.bool,
+    onChange: PropTypes.func,
+    type: PropTypes.string,
+  };
+  static defaultProps = {
+    title: 'example',
+    type: 'default',
+  };
+
+  state = {
+    checked: this.props.active,
+  };
+
+  onChange(e) {
+    this.setState({
+      checked: e.target.checked,
+    });
+    if (this.props.onChange) {
+      this.props.onChange(e);
+    }
+  }
+  render() {
+    const { title, disabled, desc, className, type, active, ...other } = this.props;
+
+    const CheckBoxContent = styled.div`
+      color: ${disabled ? style.color.placeholder : style.color.textDark};
+    `;
+
+    if (type === 'small') {
+      return (
+        <CheckBoxWrap className={className}>
+          <SmallCheckBoxView>
+            <div>
+              <CheckBoxInput
+                type="checkbox"
+                {...other}
+                disabled={disabled}
+                defaultChecked={active}
+                onChange={e => {
+                  this.onChange(e);
+                }}
+              />
+              <div className="smallcheckboxicon">
+                {this.state.checked ? (
+                  <Icon size={16} type="Confirm" />
+                ) : (
+                  <Icon size={16} type="NoConfirm" />
+                )}
+              </div>
+            </div>
+          </SmallCheckBoxView>
+        </CheckBoxWrap>
+      );
+    }
+    return (
+      <CheckBoxWrap className={className}>
+        <CheckBoxView>
+          <div>
+            <CheckBoxInput
+              type="checkbox"
+              {...other}
+              disabled={disabled}
+              defaultChecked={active}
+              onChange={e => {
+                this.onChange(e);
+              }}
+            />
+            <CheckBoxIcon className="checkboxicon" />
+          </div>
+        </CheckBoxView>
+        <CheckBoxContent>
+          {title}
+          {desc && <CheckBoxDesc>{desc}</CheckBoxDesc>}
+        </CheckBoxContent>
+      </CheckBoxWrap>
+    );
+  }
+}
 
 export default CheckBox;
--- src/routes/CheckBox/index.js
+++ src/routes/CheckBox/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, List, CheckBox, WhiteSpace } from 'pand';
+import { View, List, CheckBox, WhiteSpace, Cell } from 'pand';
 
 export default () => {
   return (
@@ -23,7 +23,16 @@ export default () => {
           <CheckBox title="name" desc="description" />
         </List.Item>
       </List>
-      <WhiteSpace />
+      <WhiteSpace>结合Cell展示协议</WhiteSpace>
+      <Cell alone padding={16}>
+        <CheckBox type="small" />
+        <Cell.Left size={12} marginLeft={6}>
+          <div>我已阅读并同意</div>
+        </Cell.Left>
+        <Cell.Main size={12} marginLeft={1} color="#666D78">
+          《熊猫金库用户服务协议》
+        </Cell.Main>
+      </Cell>
     </View>
   );
 };
