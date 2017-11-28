2972e218e9fec930d47741bf0790d4116415c009
jjj201200 <jjj201200@gmail.com>
2017-11-28 16:06:20 +0800

    split

--- src/components/style/split.js
+++ src/components/style/split.js
@@ -1,9 +1,9 @@
 import { color } from './color';
 
 export default {
-  all: `border: 1px solid ${color.split}`,
-  top: `box-shadow: 0 -1px 0 0 ${color.split}`,
-  bottom: `box-shadow: inset 0 -1px 0 0 ${color.split}`,
-  left: `border-left: 1px solid ${color.split}`,
-  right: `border-right: 1px solid ${color.split}`,
+  all: `border: solid .5px ${color.split}`,
+  top: `border-top: solid .5px ${color.split}`,
+  bottom: `border-bottom: solid .5px ${color.split}`,
+  left: `border-left: solid .5px${color.split}`,
+  right: `border-right: solid .5px ${color.split}`,
 };
