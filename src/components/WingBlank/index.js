c3f4b8276d13460505c025164c71756453e7a2c0
xiangxf <xiangxf.3504@gmail.com>
2017-12-18 19:49:09 +0800

    .

--- src/components/WingBlank/index.js
+++ src/components/WingBlank/index.js
@@ -1,12 +1,15 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import React from 'react';
 
 const props = {
   size: PropTypes.number,
 };
 
 const WingBlank = ({ size = 16, ...other }) => {
-  const WingBlank = styled.div`padding: 0 ${size}px;`;
+  const WingBlank = styled.div`
+    padding: 0 ${size}px;
+  `;
   return <WingBlank {...other} />;
 };
 
